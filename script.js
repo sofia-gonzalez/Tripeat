28b7d14448900c4632ae8f63ef26faaf17e8b3d4
sofia-gonzalez <sofia-gonzalez@users.noreply.github.com>
2018-05-21 13:03:44 +0200

    javascript suma

---
+++ script.js
@@ -0,0 +1,21 @@
+
+        function Sumar() {
+        	console.log(document.getElementById('costo1').value);
+            var n1 = document.getElementById('costo1').value;
+            var n2 = document.getElementById('costo2').value;
+            var n3 = document.getElementById('costo3').value;
+            var suma = parseInt(n1) + parseInt(n2) + parseInt(n3);
+            alert("La suma es: "+suma)
+        }
+
+var counter = 1;
+    
+        $(document).ready(function() {
+    
+            $("#addMe").click(function(){
+                counter++;
+    
+                $("#theCount").text(counter);
+            });
+    
+        });
